0819863b578b95e08bdd0f9c954ea280a65590a4
sandeep <singh.tev.san@gmail.com>
2025-11-26 18:22:47 +0530

    fetch now playing and put on store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,4 +1,21 @@
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+
+
+/*
+    Trailer Container
+        - Video in background
+        - Video title and discription
+    Movies Container
+        - Movie lists
+            - Movie Cards
+        - Series lists
+            - Series Cards
+*/
+
 const Browse = () => {
+
+    useNowPlayingMovies();
+
     return (
         <div>
             Browse Page
